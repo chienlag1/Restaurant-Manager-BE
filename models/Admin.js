fa97846346209e5adc8028d86015d1f4d930f418
XSang <sangxuan2003@gmail.com>
2025-02-24 19:19:09 +0700

    adminUpdateIMG

--- models/Admin.js
+++ models/Admin.js
@@ -6,6 +6,7 @@ const adminSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     passwordHash: { type: String, required: true },
     role: { type: String, default: "admin" },
+    avatar: { type: String, default: "/uploads/default-avatar.png" },
   },
   { timestamps: true }
 );
